d9692cab38a72a1b2a73bb5711b053f9594e5005
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2018-06-26 16:57:52 +0200

    Completed the windows screens

--- UIViews/ShoppingApp.UWP/Views/OrdersView.xaml.cs
+++ UIViews/ShoppingApp.UWP/Views/OrdersView.xaml.cs
@@ -2,18 +2,33 @@
 using MvvmCross.Platforms.Uap.Views;
 using MvvmCross.ViewModels;
 using ShoppingApp.Core.ViewModels;
-using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
 
 namespace ShoppingApp.UWP.Views
 {
     [MvxViewFor(typeof(OrdersViewModel))]
     public sealed partial class OrdersView : MvxWindowsPage
     {
-        public List<OrdersDTO> OrdersList { get; set; }
+        private OrdersViewModel PageViewModel { get { return (OrdersViewModel)ViewModel; } }
+        /// <summary>
+        /// Constructor of Order View
+        /// </summary>
         public OrdersView()
         {
             InitializeComponent();
-            this.DataContext = new OrdersViewModel();
+        }
+        /// <summary>
+        /// Click Event for Orders View Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OrdersView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as OrdersDTO;
+            if (item != null)
+            {
+                PageViewModel.ProcessOrderCommand.Execute(item);
+            }
         }
     }
 }
---
+++ UIViews/ShoppingApp.UWP/Views/ProcessOrderView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.Platforms.Uap.Views;
+using MvvmCross.ViewModels;
+using ShoppingApp.Core.ViewModels;
+
+namespace ShoppingApp.UWP.Views
+{
+    [MvxViewFor(typeof(ProcessOrderViewModel))]
+    public sealed partial class ProcessOrderView : MvxWindowsPage
+    {
+        /// <summary>
+        /// Constructor of ProcessOrder View
+        /// </summary>
+        public ProcessOrderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
